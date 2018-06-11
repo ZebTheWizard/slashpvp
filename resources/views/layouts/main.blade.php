033a3a7dfaae76a8399ee2a64be4de356b390209
Zebthewizard <burroughszeb@me.com>
2018-06-11 14:46:21 -0500

    redrose

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,12 +5,22 @@
     </head>
     <body>
         <main class="py-4" id="app">
-          <h1 class="site-title">SLASH PVP</h1>
+          @if(env('APP_NAME') === 'Slash PVP')
+            <h1 class="site-title">SLASH PVP</h1>
+          @elseif(env('APP_NAME') === 'Red Rose')
+            <div class="d-flex justify-content-center mb-2">
+              <h1 class="site-title redrose">RED</h1>
+              <img src="/img/logo-raw.png" class="my-1" width="80" height="80">
+              <h1 class="site-title redrose">ROSE</h1>
+            </div>
+
+          @endif
           <div class="w-100 text-white d-flex align-items-center justify-content-center mb-4">
             <h5 class="mx-2"><a href="/" class="text-white">Home</a></h5>
             <h5 class="mx-2"><a href="/contact" class="text-white">Contact</a></h5>
             <h5 class="mx-2"><a href="/updates" class="text-white">Updates</a></h5>
           </div>
+          <hr style="border-color: #3e3e3e;">
             @yield('content')
         </main>
         <footer class="mt-5 py-5 text-muted text-center text-small">
